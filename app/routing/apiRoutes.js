ebe4c7b3c075b2017ec8dd75af9d0793e65883aa
Sakina Shivji <shivjisakina@gmail.com>
2017-06-28 19:45:10 -0400

    did a get and post for api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,6 +3,22 @@ var path = require('path')
 
 module.exports = function (app) {
 
+    app.get('/api/workouts', function(req, res){
 
+        res.json(workouts);
 
-};
+    });
+
+    app.post('/api/workouts', function (req, res) {
+
+        console.log(req.body.q1);
+        console.log(req.body.q2);
+        console.log(req.body.q3);
+        console.log(req.body.q4);
+        console.log(req.body.q5)
+        
+    });
+
+    workouts.push(req.body);
+
+}
