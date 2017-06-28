387797ab078cc5305c0e24142d1c17b0cff21910
Sakina Shivji <shivjisakina@gmail.com>
2017-06-27 21:25:00 -0400

    commented serverjs

--- server.js
+++ server.js
@@ -4,23 +4,25 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var express = require('express');
 
+// Making my port 3000 or process.env.PORT for heroku deployment
 var PORT = process.env.PORT || 3000;
 
 var app = express();
 
-// Lets server know that its text input
+// Using body-parser to parse user input
 app.use(bodyParser.urlencoded({extended: false}));
 //app.use(methodOverride('_method'));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 
-// Use static files
+// Use all of the static files in the public folder
 app.use(express.static('app/public'));
 
-// requiring the route files 
+// Requiring the route files
 require('./app/routing/htmlRoutes.js')(app);
-require('./app/routing/apiRoutes.js');
+//require('./app/routing/apiRoutes.js')(app);
 
+// Listening to the port
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 })
