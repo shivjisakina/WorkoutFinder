529dfc74317f2b92afb1e0f1a68e65aaed0e287f
Sakina Shivji <shivjisakina@gmail.com>
2017-06-28 22:46:26 -0400

    pseudo coded in apiRoutesjs because im lost

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,22 @@
+// Pseudo code because Im lost
+
+// - require npm packages
+// - get and post /api
+// - use module exports to get it to server js
+// - get the json variables
+// - push the json variables into an object array
+// - push the array to the html(?)
+// - **do the math**
+// - push the appropriate result to html?
+
+// Requiring packages
 var workouts = require('../data/workouts.js');
 var path = require('path')
 
+// Using module.exports to export this to server.js
 module.exports = function (app) {
 
+    // getting the /api/workouts which will show the json objects
     app.get('/api/workouts', function(req, res){
 
         res.json(workouts);
@@ -11,6 +25,7 @@ module.exports = function (app) {
 
     app.post('/api/workouts', function (req, res) {
 
+        // Trying to get the questions out
         console.log(req.body.name);
         console.log(req.body.q1);
         console.log(req.body.q2);
@@ -19,7 +34,5 @@ module.exports = function (app) {
         console.log(req.body.q5)
 
     });
-
-    workouts.push(req.body);
 
 }
